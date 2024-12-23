5f50a331d5ee9f753edf3ccb9d9d6ede90f8d1ee
Nikita Maneev <nick@Nikitas-iMac.local>
2024-12-23 09:45:30 -0700

    first final look

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.getElementById("backtotop").addEventListener("click", function () {
     behavior: "smooth", // Smooth scrolling effect
   });
 });
+
+const backToTopButton = document.getElementById("backtotop");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 200) {
+    // Show button after 200px scroll
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
