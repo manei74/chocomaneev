638535160a801ba23cc1b1de623275de65f403d0
Nikita Maneev <nick@Nikitas-iMac.local>
2024-12-23 16:33:25 -0700

    add responsive design

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ window.addEventListener("scroll", function () {
     backToTopButton.style.display = "none";
   }
 });
+
+function updateVideoSource() {
+  const videoElement = document.querySelector(".myVideo");
+  const sourceElement = document.querySelector(".videoSource");
+  console.log(sourceElement);
+  console.log(window.innerWidth);
+
+  if (window.innerWidth <= 490) {
+    if (sourceElement.src !== window.location.origin + "/filem.mp4") {
+      sourceElement.src = "finalm.mp4"; // Change to smaller video
+      videoElement.load(); // Reload the video with the new source
+    }
+  } else {
+    if (sourceElement.src !== window.location.origin + "/final.mp4") {
+      sourceElement.src = "final.mp4"; // Default video
+      videoElement.load(); // Reload the video with the new source
+    }
+  }
+}
+
+// Run on page load
+window.addEventListener("load", updateVideoSource);
+
+// Run on window resize
+window.addEventListener("resize", updateVideoSource);
